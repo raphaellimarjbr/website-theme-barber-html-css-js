df8781cdd6869f257ef6b76796c14bd20215af7f
raphaellimarjbr <raphaellimaofficialrjbr@gmail.com>
2020-09-24 00:34:37 -0300

    Usando javascript para fazer o sistema de resumo da pagina sobre

--- public_html/assets/js.js
+++ public_html/assets/js.js
@@ -1,3 +1,5 @@
+/* EFFECT SCROLL MENU */
+
 window.onscroll = function()
 {
     let top = window.pageYOffset;
@@ -12,3 +14,25 @@ window.onscroll = function()
         menu.style.top = 5;
     }
 }
+
+/* TEXT SUMARY ABOUT */
+
+var text = document.querySelectorAll("p")[1].innerText;
+var textModify = document.querySelectorAll("p")[1];
+
+var number = 500;
+
+if(text.length > number){
+    var decreaseText = text.substr(0, number);
+    console.log(decreaseText);
+    textModify.innerHTML = decreaseText+"...";
+}
+
+var button = document.querySelectorAll("button")[0];
+
+button.addEventListener("click", function(){
+    if(text.length >= number){
+        console.log(text);
+        textModify.innerHTML = text;
+    }
+});
